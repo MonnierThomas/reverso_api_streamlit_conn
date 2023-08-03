cc3f45ada0b170d79bf3a4b9a9d7260a6cabbee7
Thomas Monnier <54896884+MonnierThomas@users.noreply.github.com>
2023-08-03 16:26:50 +0200

    [START] reverso api connection challenge

---
+++ reverso_app.py
@@ -0,0 +1,90 @@
+import streamlit as st
+
+from reverso_connection import ReversoConnection
+
+# Use st.experimental_connection to create the connection
+main_conn = st.experimental_connection("reverso", type=ReversoConnection)
+
+# Get the supported languages from the connection
+supported_langs = main_conn.get_supported_langs()
+
+
+# Streamlit app
+def main():
+    # Title of the app
+    st.markdown(
+        "<h1 style='text-align: center; color: blue;'>Reverso API Connection</h1>",
+        unsafe_allow_html=True,
+    )
+
+    # Use st.form to create the form
+    with st.form("ReversoForm"):
+        st.write("Enter the source and target languages and the text to be translated:")
+
+        # Source language selection
+        source_lang = st.selectbox(
+            "Source Language", ["en"] + list(supported_langs["source_lang"])
+        )
+
+        # Target language selection
+        target_lang = st.selectbox(
+            "Target Language", ["fr"] + list(supported_langs["target_lang"])
+        )
+
+        # Number of usage examples
+        n_ex = st.slider("Select the number of examples you want to see", 5, 50, 10)
+
+        # Examples list
+        examples = [
+            "Hello, how are you?",
+            "The sun is shining.",
+            "I want to go on vacation.",
+            "Life is beautiful.",
+        ]
+        example = st.radio("Choose an example...", examples, key="chosen_example")
+
+        # User input for the text to translate
+        user_input = st.text_input("...or enter a text", example)
+
+        # Submit button
+        submitted = st.form_submit_button("Translate & Get Examples!")
+
+    # Process form submission
+    if submitted:
+        # Use st.experimental_connection to create the connection
+        connection = st.experimental_connection(
+            "reverso",
+            type=ReversoConnection,
+            source_text=user_input,
+            source_lang=source_lang,
+            target_lang=target_lang,
+        )
+
+        st.session_state.is_connection = True
+
+        # Get translations and usage examples
+        translations = connection.get_usage_translations()
+        usage_examples = connection.get_usage_examples(n_ex=n_ex)
+
+        # Display translations in one column
+        with st.expander("Translations"):
+            st.dataframe(translations)
+
+        # Display usage examples in another column
+        with st.expander("Usage Examples"):
+            st.dataframe(usage_examples)
+
+    # Click on help to see what can do the connection
+    with st.expander("Connection help"):
+        st.help(main_conn)
+
+    # Click to reset the connection
+    _ = st.button("Reset the connection", key="reset")
+
+    if st.session_state.reset and st.session_state.is_connection:
+        main_conn.reset()
+        st.session_state.is_connection = False
+
+
+if __name__ == "__main__":
+    main()
---
+++ reverso_connection.py
@@ -0,0 +1,112 @@
+from itertools import islice
+
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+from reverso_api.context import ReversoContextAPI
+from streamlit.connections import ExperimentalBaseConnection
+from streamlit.runtime.caching import cache_data
+
+
+class ReversoConnection(ExperimentalBaseConnection[ReversoContextAPI]):
+    def _connect(self, **kwargs) -> ReversoContextAPI:
+        self.source_lang = kwargs.get("source_lang", "en")
+        self.target_lang = kwargs.get("target_lang", "fr")
+        self.source_text = kwargs.get("source_text", "I am awesome")
+        self.target_text = kwargs.get("target_text", "")
+
+        return ReversoContextAPI(
+            source_lang=self.source_lang,
+            target_lang=self.target_lang,
+            source_text=self.source_text,
+            target_text=self.target_text,
+        )
+
+    def instance(self) -> ReversoContextAPI:
+        return self._instance
+
+    def set_source_lang(self, source_lang: str) -> None:
+        self._instance.source_lang = source_lang
+
+    def set_target_lang(self, target_lang: str) -> None:
+        self._instance.target_lang = target_lang
+
+    def set_source_text(self, source_text: str) -> None:
+        self._instance.source_text = source_text
+
+    def set_target_text(self, target_text: str) -> None:
+        self._instance.target_text = target_text
+
+    def get_usage_translations(self, ttl: int = 3600) -> pd.DataFrame:
+        @cache_data(ttl=ttl)
+        def _get_usage_translations() -> pd.DataFrame:
+            instance = self.instance()
+            translations = instance.get_translations()
+
+            translations_dct = {}
+            for i, translation in enumerate(translations):
+                translations_dct[i] = {
+                    "translation": translation.translation,
+                    "frequency": translation.frequency,
+                    "part_of_speech": translation.part_of_speech,
+                }
+
+            df_translations = pd.DataFrame.from_dict(translations_dct, orient="index")
+            return df_translations
+
+        return _get_usage_translations()
+
+    def get_usage_examples(self, n_ex: int = 10, ttl: int = 3600) -> pd.DataFrame:
+        @cache_data(ttl=ttl)
+        def _get_usage_examples(n_ex) -> pd.DataFrame:
+            instance = self.instance()
+            examples = islice(instance.get_examples(), n_ex)
+
+            examples_dct = {}
+            for i, example in enumerate(examples):
+                source_example, target_example = example
+                examples_dct[i] = {
+                    "source": source_example.text,
+                    "target": target_example.text,
+                }
+
+            df_examples = pd.DataFrame.from_dict(examples_dct, orient="index")
+            return df_examples
+
+        return _get_usage_examples(n_ex)
+
+    def get_supported_langs(self, ttl: int = 3600) -> pd.DataFrame:
+        @cache_data(ttl=ttl)
+        def _get_supported_langs() -> pd.DataFrame:
+            supported_langs = {}
+
+            response = requests.get(
+                "https://context.reverso.net/translation/",
+                headers={
+                    "User-Agent": "Mozilla/5.0",
+                    "Content-Type": "application/json; charset=UTF-8",
+                },
+            )
+
+            soup = BeautifulSoup(response.content, features="lxml")
+
+            src_selector = soup.find("div", id="src-selector")
+            trg_selector = soup.find("div", id="trg-selector")
+
+            for selector, attribute in (
+                (src_selector, "source_lang"),
+                (trg_selector, "target_lang"),
+            ):
+                dd_spans = selector.find(class_="drop-down").find_all("span")
+                langs = [span.get("data-value") for span in dd_spans]
+                langs = [
+                    lang for lang in langs if isinstance(lang, str) and len(lang) == 2
+                ]
+
+                supported_langs[attribute] = tuple(langs)
+            
+            df_supported_langs = pd.DataFrame.from_dict(supported_langs)
+
+            return df_supported_langs
+
+        return _get_supported_langs()
